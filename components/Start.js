8a96e329e07b7bb267e2ae8eb05e1a083ce3919a
Val Pilossof <valpil610@gmail.com>
2018-05-07 12:59:32 -0400

    ADD: added timer and exit button components

--- components/Start.js
+++ components/Start.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { StackNavigator } from 'react-navigation';
+import { Button } from 'react-native-elements';
+import Timer from './Timer.js';
 
 const Start = ({ navigation }) => (
   <View style={styles.container}>
+    <Timer />
     <Text style={styles.text}>To begin the game:</Text>
     <Text style={styles.text}>
       Hold your screen up to your face, pointing your phone's camera toward the
