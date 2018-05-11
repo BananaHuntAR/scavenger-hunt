67fd47fb1400ae51e4d921711c5341cfa72da138
Marshall Zobel <dmzobel@gmail.com>
2018-05-11 15:46:51 -0400

    REFAC: Home screen icons moved into Toolbar

--- components/Home.js
+++ components/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Text } from 'react-native-elements';
-import { EvilIcons, Ionicons } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
 import { connect } from 'react-redux';
 import { logout } from '../store/auth';
 import Toolbar from './Toolbar';
@@ -15,44 +15,25 @@ const Home = ({ navigation, currentUser, logoutFunc }) => (
       <Text h4 style={styles.headerText}>
         An Augmented Reality Scavenger Hunt
       </Text>
-      <EvilIcons
-        name="play"
+      <FontAwesome
+        name="play-circle"
         size={125}
         onPress={() => navigation.navigate('Game')}
         style={styles.icon}
       />
-      <Text style={styles.iconText}>Play!</Text>
     </View>
 
-    <View style={styles.iconContainer}>
-      <View>
-        <Ionicons
-          name="ios-clipboard-outline"
-          size={50}
-          onPress={() => navigation.navigate('Instructions')}
-          style={styles.icon}
-        />
-        <Text style={styles.iconText}>Tutorial</Text>
-      </View>
-      <View>
-        <EvilIcons
-          name="trophy"
-          size={60}
-          onPress={() => navigation.navigate('Leaderboard')}
-          style={styles.icon}
-        />
-        <Text style={styles.iconText}>Leaderboard</Text>
-      </View>
-    </View>
     <Text
       onPress={() => {
         return currentUser.email
           ? logoutFunc(navigation)
           : navigation.navigate('Login');
       }}
+      style={styles.loginText}
     >
-      Log In
+      Login
     </Text>
+
     <Toolbar />
   </View>
 );
@@ -77,21 +58,16 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     padding: 10
   },
-  iconText: {
+  loginText: {
     color: 'white',
     textAlign: 'center',
+    padding: 20,
     fontSize: 16
   },
   icon: {
     color: 'white',
     paddingTop: 10,
     alignSelf: 'center'
-  },
-  iconContainer: {
-    flexDirection: 'row',
-    paddingTop: 25,
-    alignItems: 'flex-end',
-    justifyContent: 'space-evenly'
   }
 });
 
--- components/Toolbar.js
+++ components/Toolbar.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { withNavigation } from 'react-navigation';
 import { Button, Icon, Fab } from 'native-base';
+import { Feather, EvilIcons, Ionicons } from '@expo/vector-icons';
 
-export default class Toolbar extends Component {
+class Toolbar extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -16,20 +18,36 @@ export default class Toolbar extends Component {
         direction="right"
         containerStyle={{}}
         style={{ backgroundColor: '#3FBE94' }}
-        position="topLeft"
+        position="bottomLeft"
         onPress={() => this.setState({ active: !this.state.active })}
       >
-        <Icon name="md-settings" />
-        <Button style={{ backgroundColor: '#34A34F' }}>
-          <Icon name="trophy" />
+        <Icon name="md-more" />
+        <Button style={{ backgroundColor: '#fced4e' }}>
+          <Feather name="user" size={21} />
         </Button>
-        <Button style={{ backgroundColor: '#3B5998' }}>
-          <Icon name="logo-facebook" />
+        <Button
+          style={{ backgroundColor: '#fced4e' }}
+          onPress={() => this.props.navigation.navigate('Leaderboard')}
+        >
+          <EvilIcons
+            name="trophy"
+            size={30}
+            style={{ color: 'black', fontWeight: 'bold' }}
+          />
         </Button>
-        <Button disabled style={{ backgroundColor: '#DD5144' }}>
-          <Icon name="mail" />
+        <Button
+          style={{ backgroundColor: '#fced4e' }}
+          onPress={() => this.props.navigation.navigate('Instructions')}
+        >
+          <Ionicons
+            name="ios-clipboard-outline"
+            size={25}
+            style={{ color: 'black' }}
+          />
         </Button>
       </Fab>
     );
   }
 }
+
+export default withNavigation(Toolbar);
