53da67f33816c65fa6da687b66d14c5687ed4d5e
andrew wong <awwong92@gmail.com>
2018-05-11 12:55:46 -0400

    ADD: create view for game options, added buttons

---
+++ components/GameOptionPage.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { StyleSheet, View } from 'react-native';
+import { Button } from 'react-native-elements';
+
+const GameOptionPage = ({ navigation }) => (
+  <View style={styles.container}>
+    <View>
+      <Button
+        style={{ width: 150 }}
+        onPress={() => navigation.navigate('Home')}
+        raised
+        rounded
+        title='Quick Random Game'
+        backgroundColor="white"
+        color="gray"
+      />
+      <Button
+        style={{ width: 150 }}
+        onPress={() => navigation.navigate('Home')}
+        raised
+        rounded
+        title='Custom Games'
+        backgroundColor="white"
+        color="gray"
+      />
+      <Button
+        style={{ width: 150 }}
+        onPress={() => navigation.navigate('Home')}
+        raised
+        rounded
+        title='Create a Custom Game'
+        backgroundColor="white"
+        color="gray"
+      />
+    </View>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#3FBE94',
+    flex: 1,
+    paddingBottom: 100
+  },
+  instructionsContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    borderRadius: 20,
+    padding: 10
+  },
+  text: {
+    textAlign: 'center',
+    justifyContent: 'center',
+    alignSelf: 'center',
+    color: 'white',
+    padding: 10,
+    fontFamily: 'OriyaSangamMN'
+  }
+});
+
+export default connect(GameOptionPage);
